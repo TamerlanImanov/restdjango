bafa12c4e3c3f3897224e4631b76cd550699ec97
TamerlanImanov <tamerlan.imanov@ce.sdu.edu.kz>
2015-03-09 18:10:51 +0600

    creating urls

--- restdjango/urls.py
+++ restdjango/urls.py
@@ -1,10 +1,17 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'restdjango.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+from question.api import *
 
-    url(r'^admin/', include(admin.site.urls)),
+post_res = PostResource()
+comment_res = CommentResource()
+
+urlpatterns = patterns('',
+  # Examples:
+  # url(r'^$', 'rest_project.views.home', name='home'),
+  # url(r'^blog/', include('blog.urls')),
+
+  url(r'^admin/', include(admin.site.urls)),
+  url(r'^api/v1/', include(post_res.urls)),
+  url(r'^api/v1/', include(comment_res.urls)),
 )
