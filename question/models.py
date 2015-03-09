cf7d85b02f985d7bde842ca86d64a224eeb8d2c0
TamerlanImanov <tamerlan.imanov@ce.sdu.edu.kz>
2015-03-09 17:00:52 +0600

    creating models

--- question/models.py
+++ question/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+class Post(models.Model):
+	author  = models.CharField(max_length=100)
+	tittle = models.CharField(max_length=100)
+	text = models.CharField(max_length=1000)
+	pub_date = models.DateTimeField(default=timezone.now)
+	upd_date = models.DateTimeField(default=timezone.now)
+	is_public = models.BooleanField(default=False)
+
+class Comment(models.Model):
+	author = models.CharField(max_length=100)
+	text = models.CharField(max_length=1000)
+	pub_date = models.DateTimeField(default=timezone.now)
+	post = models.ForeignKey(Post)
